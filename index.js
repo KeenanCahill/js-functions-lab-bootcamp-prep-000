6ab801d2bd667f4302256a9b6cc88efb10c84322
Keenan Cahill <thatkeenancahill@gmail.com>
2019-04-29 06:09:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ return "Happy Holidays!"
 function happyHolidaysTo(name) {
 return "Happy Holidays ,${name} !"
 }
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`}
